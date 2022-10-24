d078a57a909cfba45f2d60394287b9f9451674c5
Nikita <nikidevkode@gmail.com>
2022-10-24 20:38:14 +0200

    add feature 1 add second commit

--- app/src/main/java/com/example/currenciesexchange/MainActivity.kt
+++ app/src/main/java/com/example/currenciesexchange/MainActivity.kt
@@ -35,6 +35,12 @@ fun Greeting21(name: String) {
   Text(text = "Hello $name!")
 }
 
+
+@Composable
+fun Greeting12(name: String) {
+  Text(text = "Hello $name!")
+}
+
 @Composable
 fun Greeting(name: String) {
   Text(text = "Hello $name!")
